451404bb9fb64bf358fad06aef693bc52e7941cb
Santtu Lintervo <santervo@gmail.com>
2011-02-08 21:09:39 +0200

    project skeleton

---
+++ src/clj_extensions/core.clj
@@ -0,0 +1,1 @@
+(ns clj-extensions.core)
---
+++ test/clj_extensions/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-extensions.test.core
+  (:use [clj-extensions.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
